93d426d2d991cee386060e91f1a5713e1d5e046a
Plínio Cunha <pliniocunha@hotmail.com>
2018-02-03 22:46:11 -0300

    Try

--- LoteriaConsole/Program.cs
+++ LoteriaConsole/Program.cs
@@ -9,31 +9,38 @@ namespace LoteriaConsole
     {
         static void Main(string[] args)
         {
-            var megaSena = new MegaSena();
-            for (var i = 0; i < 30000; i++)
+            try
             {
-                megaSena.RegistrarJogo(megaSena.GerarNumerosAleatorios());
-                megaSena.RegistrarJogoAutomatico();
-            }
-
-            var numerosSorteados = megaSena.GerarNumerosAleatorios();
-            var ganhadores = megaSena.Acertadores((List<int>)numerosSorteados);
-
-            Console.WriteLine($"Números sorteados: {string.Join(",", numerosSorteados.OrderBy(a => a).ToList())}.");            
-            Console.WriteLine();
-
-            foreach (var item in ganhadores)
-            {
-                Console.WriteLine($"Dezendas: {item.Key} - Número de bilhetes premiados: {item.Value.Count()}");
-                foreach (var n in item.Value)
+                var megaSena = new MegaSena();
+                for (var i = 0; i < 30000; i++)
                 {
-                    Console.WriteLine($"Bilhete: {n.Id.ToString().PadLeft(6, '0')} - {n.DataHoraJogo} - {string.Join(",", n.GetNumeros().OrderBy(a => a).ToList())}");
+                    megaSena.RegistrarJogo(megaSena.GerarNumerosAleatorios());
+                    megaSena.RegistrarJogoAutomatico();
                 }
 
-                Console.WriteLine();
-            }
+                var numerosSorteados = megaSena.GerarNumerosAleatorios();
+                var ganhadores = megaSena.Acertadores((List<int>)numerosSorteados);
 
-            Console.ReadKey();
+                Console.WriteLine($"Números sorteados: {string.Join(",", numerosSorteados.OrderBy(a => a).ToList())}.");
+                Console.WriteLine();
+
+                foreach (var item in ganhadores)
+                {
+                    Console.WriteLine($"Dezendas: {item.Key} - Número de bilhetes premiados: {item.Value.Count()}");
+                    foreach (var n in item.Value)
+                    {
+                        Console.WriteLine($"Bilhete: {n.Id.ToString().PadLeft(6, '0')} - {n.DataHoraJogo} - {string.Join(",", n.GetNumeros().OrderBy(a => a).ToList())}");
+                    }
+
+                    Console.WriteLine();
+                }
+
+                Console.ReadKey();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
         }
     }
     
